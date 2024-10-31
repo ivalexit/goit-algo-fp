bd95d8575a213396160e7f663d3b668344ec269b
ivalexit <ivalextar@gmail.com>
2024-10-31 16:17:42 +0100

    def insert at begin

--- fp_1.py
+++ fp_1.py
@@ -7,3 +7,8 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
